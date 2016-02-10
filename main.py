bbf1f3b34743d1b80dc865eb8360d55660abf755
Paul Kalauner <pkalauner@student.tgm.ac.at>
2016-02-10 07:55:35 +0100

    removed qt import

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 """
 Autor: Paul Kalauner 5BHIT
 """
-import sys
 from CsvExampleController import CsvExampleController
-from PySide.QtGui import QApplication
 
 if __name__ == "__main__":
     c = CsvExampleController()
